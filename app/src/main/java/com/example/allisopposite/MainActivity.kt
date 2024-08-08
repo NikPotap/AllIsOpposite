4f43212b5bf2336b0b23cbc29814836b0d666364
Nikita <github.com@potapkin.com>
2024-08-08 14:57:28 +0300

    Проект в работающем варианте.

--- app/src/main/java/com/example/allisopposite/MainActivity.kt
+++ app/src/main/java/com/example/allisopposite/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.allisopposite
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputText: EditText
+    private lateinit var resultOfConvert: TextView
+    private lateinit var symbolsOnResult: TextView
+    private lateinit var buttonConvert: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        inputText = findViewById(R.id.inputText)
+        resultOfConvert = findViewById(R.id.resultOfConvert)
+        symbolsOnResult = findViewById(R.id.symbolsOnResult)
+        buttonConvert = findViewById(R.id.buttonConvert)
+
+        clickToButton(buttonConvert)
+    }
+
+    fun clickToButton(view: View) {
+        val text = inputText.text
+        resultOfConvert.text = text.reversed()
+        symbolsOnResult.text = (text.filterNot { it == ' ' }).length.toString()
     }
 }
